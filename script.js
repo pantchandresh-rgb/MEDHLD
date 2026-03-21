9ae132da8ea4974342aee7a603d19a50b7f09bf0
pantchandresh-rgb <pant.chandresh@gmail.com>
2026-03-21 18:35:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -191,3 +191,26 @@ function calculatePrice() {
     document.getElementById("estimateResult").innerText =
         "Estimated Price: ₹" + price.toLocaleString("en-IN");
 }
+
+// HERO IMAGE SLIDER
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+function autoSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+// Show first slide
+showSlide(currentSlide);
+
+// Auto change every 3 sec
+setInterval(autoSlide, 3000);
